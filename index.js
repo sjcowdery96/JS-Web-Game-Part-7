b4efdee5c2a3792080078d34a646494042e3be5c
sjcowdery96 <sjc.cowdery@gmail.com>
2023-09-23 14:10:18 -0500

    completed infinite loop for npc

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
-npc.walkEast()
+async function moveNPC(){
+    npc.walkEast(2000)
+    await npc.walkNorth(2000)
+    await npc.walkWest(3000)
+}
+
+moveNPC()
 
 // Create the inventory
 const inventory = newInventory()
--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -23,29 +23,44 @@ function newNonPlayableCharacter(x, y) {
 
     setInterval(moveCharacter, 1)
 
-    function walkEast() {
+    async function walkEast(time) {
+
+        //added async keyword, time duration as a param, and async 
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
+        await sleep(time)
+        stop()
     }
 
-    function walkNorth() {
+    async function walkNorth(time) {
         direction = 'north'
-        element.src = `./assets/red-character/north.gif`
+        element.src = `./assets/red-character/north.gif` 
+        await sleep(time)
+        stop()
     }
 
-    function walkWest() {
+    async function walkWest(time) {
         direction = 'west'
         element.src = `./assets/red-character/west.gif`
+        await sleep(time)
+        stop()
     }
 
-    function walkSouth() {
+    async function walkSouth(time) {
         direction = 'south'
         element.src = `./assets/red-character/south.gif`
+        await sleep(time)
+        stop()
     }
 
     function stop() {
         direction = null
         element.src = `./assets/red-character/static.gif`
+    }
+
+    function sleep(){
+
+        return
     }
 
     return {
